c165b6395c5d008312b686fa7f297a84f29a4e86
siva23 <tekisiva2012@gmail.com>
2016-02-16 16:50:30 +0530

    first commit

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,27 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+var app = angular.module("jsapp", ['ngRoute','ngResource']);
+
+app.controller('ProductsController', function($scope, $http){
+	console.log("controller called");
+	$scope.name = 'hi';
+	$scope.products = [];
+	$http.get('/products.json').then(
+			function(data){
+				console.log("-works-");
+				$scope.products = data["data"];
+			},
+			function(e){
+				console.log("-error-");
+				console.log(e);
+			}
+		)
+});
+
+app.config(['$routeProvider', function($routeProvider){
+		$routeProvider.when('/', {templateUrl: 'templates/products.html', controller: 'ProductsController'})
+	}
+]);
+
+
